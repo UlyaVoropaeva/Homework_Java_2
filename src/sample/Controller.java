f04be548d713694116dfd9579a941404b648646c
Ulya <Ulya2811@yandex.ru>
2021-04-19 22:03:15 +0300

    lesson_4_JavaFx

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,9 +1,7 @@
 package sample;
 
 import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
